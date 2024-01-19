ebe2ebebdfe829146fe1c6875fe1edb1bdac784c
jcob-yim <142728227+jcob-yim@users.noreply.github.com>
2024-01-19 12:13:27 +0900

    Add files via upload
    main 처음 업데이트

---
+++ src/githubEx1.java
@@ -0,0 +1,17 @@
+package githubEx;
+
+public class githubEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// git(로컬저장소)에만 있던 원시 파일
+		
+		if (true) {
+			System.out.println("첫번째 main 수정!");
+		} else {
+			System.out.println("첫번째 main 수정!");
+		}
+	}
+
+}
